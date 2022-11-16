92d9de539b0446258c83f333d0ee4f4052786000
bazubagir <bazubedinah@gmail.com>
2022-11-16 14:16:09 +0200

    header file

---
+++ components/header.tsx
@@ -0,0 +1,40 @@
+import { SearchIcon, BellIcon } from "@heroicons/react/solid";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <img
+          src="https://images.ctfassets.net/4cd45et68cgf/7LrExJ6PAj6MSIPkDyCO86/542b1dfabbf3959908f69be546879952/Netflix-Brand-Logo.png?w=684&h=456"
+          alt=""
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">Tv Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
+        </ul>
+      </div>
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <SearchIcon className="hidden w-6 h-6 sm:inline" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className=" h-6 w-6" />
+        <Link href="/account">
+          <img
+            src="https://rb.gy/g1pwyx"
+            alt=""
+            className="cursor-pointer rounded"
+          />
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
